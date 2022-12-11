8a72fe657612cb4e1d81c3e9e22ca0904c7fffc8
Marcos Paulo Cunha Martins <marcos.martins.ext@dasa.com.br>
2022-12-11 12:23:22 -0300

    finalizando validações do formulario e custom hooks

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,33 +1,110 @@
 import PropTypes from 'prop-types';
-import FormGroup from '../FormGroup';
+import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../hooks/useErrors';
+
 import { Form, ButtonContainer } from './styles';
+
+import FormGroup from '../FormGroup';
 import { Input } from '../Input';
 import { Select } from '../Select';
 import { Button } from '../Button';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const {
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+    errors,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
+
+  function handleNameChange(event) {
+    const { value } = event.target;
+    setName(value);
+
+    if (!value) {
+      setError({ field: 'name', message: 'O nome é obrigatório.' });
+    } else {
+      removeError({ fieldName: 'name' });
+    }
+  }
+
+  function handleEmailChange(event) {
+    const { value } = event.target;
+    setEmail(value);
+
+    if (value && !isEmailValid(value)) {
+      setError({ field: 'email', message: 'Email inválido.' });
+    } else {
+      removeError({ fieldName: 'email' });
+    }
+  }
+
+  function handlePhoneChange(event) {
+    const { value } = event.target;
+    setPhone(formatPhone(value));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name, email, phone: phone.replace(/\D/g, ''), category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" type="text" />
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          placeholder="Nome *"
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
 
-      <FormGroup error="O formato do email é inválido">
-        <Input placeholder="E-mail" type="text" error />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" type="text" />
+        <Input
+          placeholder="Telefone"
+          type="text"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
